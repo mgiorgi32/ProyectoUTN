d1eafc00b569410db261ef581e6009b26b7f5ff3
mgiorgi32 <marcosgiorgi2004@gmail.com>
2024-03-20 21:07:16 -0300

    agregamos los ultimos 3 cuadrados de artistas, limpiamos el codigo y por ultimo empezamos la pagina TOP

---
+++ JS/top.js
@@ -0,0 +1,15 @@
+function IrAlTopIG(){
+    window.open('https://www.instagram.com/billieeilish/', '_blank');
+}
+function IrAlTopRecital(){
+    window.open('https://youtu.be/XkFhEoXnMrc?si=AKDt_cGBPCjBG_gR', '_blank');
+}
+function IrAlTopSpotify(){
+    window.open('https://open.spotify.com/intl-es/artist/6qqNVTkY8uBg9cP3Jd7DAH', '_blank');
+}
+function IrAlTopTW(){
+    window.open('https://twitter.com/billieeilish?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor', '_blank');
+}
+function IrAlTopYT(){
+    window.open('https://www.youtube.com/channel/UCiGm_E4ZwYSHV3bcW1pnSeQ', '_blank');
+}
